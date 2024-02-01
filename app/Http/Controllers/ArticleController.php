bc5c5f9154088a3d237684b9dd4471f5c106b32f
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-02-02 00:02:11 +0530

    make change in ArticleController and use it in api.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,25 +10,21 @@ class ArticleController extends Controller
     public function index(){
         return Article::all();
     }
-    public function show($id){
-        return Article::find($id);
-    }
-    public function store(Request $request){
-        return Article::created($request->all());
-    }
-
-    public function update(Request $request,$id){
-        $article=Article::findOrFail($id);
-        $article->update($request->all());
-
+    public function show(Article $article){
         return $article;
     }
-
-    public function delete(Request $request,$id){
-        $article=Article::findOrFail($id);
-        $article->delete();
-
-        return 204;
+    public function store(Request $request){
+        $article= Article::create($request->all());
+        return response()->json($article,201);
     }
 
+    public function update(Request $request,Article $article){
+        $article->update($request->all());
+        return response()->json($article,200);
+    }
+
+    public function delete(Article $article){
+        $article->delete();
+        return response()->json(null,204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Article;
 Route::get('articles','ArticleController@index');
 
-Route::get('articles/{id}',function($id){
-    return Article::find($id);
-});
+Route::get('articles/{article}','ArticleController@show');
 
-Route::post('articles',function(Request $request,$id){
-    $article=Article::findOrFail($id);
-    $article->update($request->all());
-    return $article;
-});
-Route::put('article/{id}',function  (Request $request,$id){
-    $article=Article::findOrFail($id);
-    $article->update($request->all());
-    return $article;
-});
-Route::delete('articals/{id}',function($id){
-    Article::find($id)->delete();
-    return 204;
-});
+Route::post('articles','ArticleController@store');
+
+Route::put('article/{article}','ArticleController@update');
+
+Route::delete('articals/{article}','ArticleController@delete');
